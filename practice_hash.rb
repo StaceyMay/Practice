c1d008939bf3a64bafe9a274ccbbe4ce60ae1b43
staceymay <missstaceymay@gmail.com>
2016-11-07 18:52:21 -0800

    oldest to youngest

--- practice_hash.rb
+++ practice_hash.rb
@@ -1,3 +1,6 @@
+#sort pople by age   
+
+
   age_hash = {"Carl" => 22, "Abe" => 23, "Barbara" => 25, "Duke" => 70 }
 
   # sorted_age =  age_hash.keys.sort
@@ -6,10 +9,16 @@
 
   sorted_age = age_hash.sort_by {|name,age| age}
 
-  p sorted_age
+  puts "Here is everyone from youngest to oldest:"
 
   sorted_age.each do |key, value|
     puts "#{key} is #{value} years old"
   end
 
+oldest_age = age_hash.sort_by {|name,age| age}.reverse
 
+    puts "Here is everyone from oldest to youngest:"
+
+  oldest_age.each do |key, value|
+    puts "#{key} is #{value} years old"
+  end
